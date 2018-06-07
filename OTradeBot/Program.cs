87151714a62f3fdd0bc5e1973d43ea98df2df347
Артём <anzer987@yandex.ru>
2018-06-07 15:25:20 +0300

    test

---
+++ OTradeBot/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace OTradeBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestChrome testchrome = new TestChrome();
+            testchrome.Start();
+            Environment.Exit(0);
+        }
+    }
+}
